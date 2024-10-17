5ee0fed4b41236ee44a6dbfcd4c02a0ebd8644c1
SrZeoth <94896531+SrZeoth@users.noreply.github.com>
2024-10-17 22:35:50 +0200

    start algorithm setup

--- Assets/Scripts/ScriptableObjects/Character.cs
+++ Assets/Scripts/ScriptableObjects/Character.cs
@@ -10,7 +10,29 @@ public class Character : ScriptableObject
     public string name;
     public int maxHp;
     public int maxSp;
-    public int atk;
-    public int def;
+    public int strength;
+    public int resilience;
+    public int intelligence;
+    public int mind;
+    public int agility;
+    public int critChance;
+    public int critDamage;
+    public DamageType damageType;
+    public Element element;
     public List<Skill> skills = new List<Skill>();
+        public enum DamageType
+    {
+        Slash,
+        Thrust,
+        Blunt,
+        Magical
+    }
+    public enum Element
+    { 
+        Fire,
+        Water,
+        Earth,
+        Wind,
+        Neutral
+    } 
 }
