7a3b4ee54d1b6571191432933e8f8c1b48f947f3
LightAi39 <infowesselvanhemert@gmail.com>
2025-01-23 12:59:52 +0100

    only build editor script in editor

--- Assets/Scripts/UnityEditorUI/DamageTypeWeaknessEditor.cs
+++ Assets/Scripts/UnityEditorUI/DamageTypeWeaknessEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -99,3 +100,4 @@ public class DamageTypeWeaknessEditor : Editor
         DrawDefaultInspector();
     }
 }
+#endif
